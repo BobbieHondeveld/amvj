f9f885c4f1eeeaca9e0877061be2c732f56f93bd
Jevon van Dijk <jevon@atabix.nl>
2014-09-26 11:59:45 +0200

    gem install and autoprefixer on update
    Signed-off-by: Jevon van Dijk <jevon@atabix.nl>

--- static/admin/Gruntfile.js
+++ static/admin/Gruntfile.js
@@ -1,18 +1,60 @@
+
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
     sass: {
       options: {
-        includePaths: ['../components/foundation/scss']
+        includePaths: ['components/foundation/scss']
       },
       dist: {
         options: {
-          outputStyle: 'expanded'
+          outputStyle: 'compressed'
         },
         files: {
           'css/style.css': 'scss/style.scss'
         }        
+      }
+    },
+
+    cssmin: {
+      combine: {
+        files: {
+          'css/mi.min.css': [
+
+            'components/foundation/css/foundation.css',
+
+            '../../vendor/datatables/datatables/media/css/jquery.dataTables.min.css', 
+            'css/foundation-icons.css',
+            'css/style.css'
+          ]
+        }
+      }
+    },
+
+    uglify: {
+      my_target: {
+        options: {
+        },
+        files: {
+          'js/mi.min.js': [
+            'components/jquery/dist/jquery.min.js',
+            
+            'components/foundation/js/vendor/fastclick.js',            
+            'components/foundation/js/vendor/jquery.cookie.js',            
+            'components/foundation/js/vendor/jquery.js',            
+            'components/foundation/js/vendor/modernizr.js',            
+            'components/foundation/js/vendor/placeholder.js',            
+
+            'components/foundation/js/foundation.min.js',
+
+            'components/jquery-tokeninput/build/jquery.tokeninput.min.js',
+
+            '../../vendor/datatables/datatables/media/js/jquery.dataTables.min.js',
+            'js/cssmenu.js',
+            'js/main.js'
+          ]
+        }
       }
     },
 
@@ -28,9 +70,10 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('build', ['sass']);
+  grunt.registerTask('build', ['sass', 'cssmin', 'uglify']);
   grunt.registerTask('composer', ['build']);
   grunt.registerTask('default', ['build','watch']);
 }
