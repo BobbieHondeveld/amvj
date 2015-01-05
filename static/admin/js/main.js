bc59c9ddbe31608a9d36b6bdfdbf8cae736a2757
Paul Hage <paul.hage@atabix.com>
2015-01-06 00:03:45 +0100

    Deleted admin folder + deleted index.php
    Signed-off-by: Paul Hage <paul.hage@atabix.com>

--- static/admin/js/main.js
+++
@@ -1,6 +0,0 @@
-// Foundation JavaScript
-// Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
-
-var doc = document.documentElement;
-doc.setAttribute('data-useragent', navigator.userAgent);
