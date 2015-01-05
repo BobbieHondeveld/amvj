30985a1ab7948ec757629a2e1b0c39e7f2e6d779
Paul Hage <paul.hage@atabix.com>
2015-01-06 00:03:19 +0100

    Update Deploy Folder
    Signed-off-by: Paul Hage <paul.hage@atabix.com>

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,7 +1,7 @@
 cache_release_path = release_path
 bash "installing atabase environment" do
   user "deploy"
-  group "www-data"
+  group "apache"
   #cwd ::File.join(cache_release_path, 'deploy')
   cwd cache_release_path
   code <<-EOH
@@ -10,25 +10,12 @@ bash "installing atabase environment" do
     cp ./deploy/index.php ./index.php
     curl -sS https://getcomposer.org/installer | php -- --filename=composer
     chmod 0775 .
-    mkdir conf
-    chmod 0775 ./conf
-    sudo chown deploy:www-data ./conf
-    mkdir tmp
     chmod 0775 ./tmp
+    mkdir conf
+    chmod -R 0775 ./conf
     mkdir content
-    mkdir content/invoices
-    mkdir content/products
-    mkdir content/products/originals
-    mkdir content/products/views
-    mkdir content/products/thumbnails
-    mkdir content/products/minis
-    mkdir content/dafproduct_imgs
-    mkdir content/brands
-    mkdir content/brands/logos
-    mkdir content/brands/headers
-    mkdir content/brands/media
-    mkdir content/Stores
     chmod -R 0775 ./content
     ./composer update
+    chmod 0775 ./vendor/spekkionu/htmlpurifier/HTMLPurifier/DefinitionCache/Serializer
   EOH
 end
--- deploy/index.php
+++ deploy/index.php
@@ -1,5 +1,7 @@
 <?php
 
+date_default_timezone_set("Europe/Amsterdam");
+
 use Atabix\Core as Atabase;
 
 /*****************************************/
@@ -12,9 +14,6 @@ try {
 
 	$bootstrapper=new Atabase\Bootstrapper($kernel, $baseDir);
 	$bootstrapper->init();
-	global $CONFIG;
-	$CONFIG->setValue("general", "baseDir", $baseDir);
-
 } catch(PDOException $e) {
 	$logger = new Monolog\Logger('database');
 	$logger->pushHandler(new Monolog\Handler\StreamHandler($baseDir.'/tmp/database.log', Monolog\Logger::DEBUG));
@@ -23,7 +22,7 @@ try {
 	header(Atabase\Exceptions\HTTPStatusLookup::httpHeaderFor(500));
 	echo Atabase\Exceptions\HTTPStatusLookup::getMessageForCode(500).' - Database Connection Error';
 	exit();
-} catch(HTTPErrorException $e) {
+} catch(Atabase\Exceptions\HTTPErrorException $e) {
 	$logger = new Monolog\Logger('kernel');
 	$logger->pushHandler(new Monolog\Handler\StreamHandler($baseDir.'/tmp/kernel.log', Monolog\Logger::DEBUG));
 	$logger->addCritical($e->getMessage());
@@ -37,16 +36,16 @@ try {
 /*****************************************/
 try {
 	$router=new Atabase\RequestRouter($baseDir);
-	$router->addNamespace('Atabix');
-	$router->addDispatcher(new Atabase\Route('Atabix\Website\PublicController', '/', 'dispatcher1'));
-	
+	$router->addNamespace('Cloud');
+    $router->setExceptionRedirect(403, "/login");
+    
 	$result=$router->routeRequest($_SERVER['REQUEST_URI']);
+
 	
+	header($result['header']);
 	if($result['output']=='json') {
-    	header($result['header']);
         echo json_encode($result['body']);
 	} elseif($result['output']=='echo') {
-    	header($result['header']);
         echo $result['body'];
 	} else {
     	// No output
